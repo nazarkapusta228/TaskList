6666dcc0a0214bd9c23dc9e2ad4212619a5f3849
Nazar Kapustynskyi <258522@edu.p.lodz.pl>
2025-10-27 23:55:58 +0100

    declared fields, created constructors, implemented getters and setters in model/Task

--- src/main/java/com/nazarkapusta228/tasklist/model/Task.java
+++ src/main/java/com/nazarkapusta228/tasklist/model/Task.java
@@ -1,4 +1,73 @@
 package com.nazarkapusta228.tasklist.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private boolean isDone;
+    private LocalDate deadline;
+
+    public Task() {}
+
+    public Task(String title, String description, LocalDate deadline, boolean done){
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.isDone = done;
+    }
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+
+    public boolean IsDone(){
+        return isDone;
+    }
+
+    public void setIsDone(boolean isDone){
+        this.isDone = isDone;
+    }
+
+    public LocalDate getDeadline(){
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline){
+        this.deadline = deadline;
+    }
+
+
+
+
+
 }
