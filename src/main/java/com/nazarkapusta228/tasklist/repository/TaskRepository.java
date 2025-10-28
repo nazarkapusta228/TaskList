12be6cc6c9e2de5cdd6a7c1d150c3632648183e1
Nazar Kapustynskyi <258522@edu.p.lodz.pl>
2025-10-28 20:53:00 +0100

    declared methods in TaskRepository

--- src/main/java/com/nazarkapusta228/tasklist/repository/TaskRepository.java
+++ src/main/java/com/nazarkapusta228/tasklist/repository/TaskRepository.java
@@ -1,4 +1,15 @@
 package com.nazarkapusta228.tasklist.repository;
 
+import com.nazarkapusta228.tasklist.model.Task;
+
+import java.util.List;
+
+
 public interface TaskRepository {
+    public void addTask(Task task);
+    public void removeTask(int id);
+    public void updateTask(Task task);
+    public boolean isDone(int id);
+    public List<Task> findTasksByTitle(String titlePart);
+    public List<Task> findTasksByDoneStatus(boolean done); ;
 }
