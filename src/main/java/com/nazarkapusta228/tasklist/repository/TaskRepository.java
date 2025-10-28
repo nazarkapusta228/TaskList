324ab99bdf4f910c68ba0661f0dbbbd3a2c57551
Nazar Kapustynskyi <258522@edu.p.lodz.pl>
2025-10-28 21:48:21 +0100

    Implement addTask() method and a few helper methods | Add new method to TaskRepository

--- src/main/java/com/nazarkapusta228/tasklist/repository/TaskRepository.java
+++ src/main/java/com/nazarkapusta228/tasklist/repository/TaskRepository.java
@@ -11,5 +11,5 @@ public interface TaskRepository {
     public void updateTask(Task task);
     public boolean isDone(int id);
     public List<Task> findTasksByTitle(String titlePart);
-    public List<Task> findTasksByDoneStatus(boolean done); ;
+    public List<Task> findTasksByDoneStatus(boolean done);
 }
--- src/main/java/com/nazarkapusta228/tasklist/service/TaskService.java
+++ src/main/java/com/nazarkapusta228/tasklist/service/TaskService.java
@@ -1,4 +1,86 @@
 package com.nazarkapusta228.tasklist.service;
 
-public class TaskService {
+import com.nazarkapusta228.tasklist.model.Task;
+import com.nazarkapusta228.tasklist.repository.TaskRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskService implements TaskRepository {
+    private List<Task> tasksList = new ArrayList<>();
+
+
+    @Override
+    public void addTask(Task task){
+        if (checkTask(task) & !checkIfTaskWasAdded(task)) {
+            tasksList.add(task);
+            System.out.println("Task was successfully added");
+        }
+    }
+
+
+    @Override
+    public void removeTask(int id){
+
+    }
+
+    @Override
+    public void updateTask(Task task){
+
+    }
+
+    @Override
+    public boolean isDone(int id){
+        return true;
+    }
+
+    @Override
+    public List<Task> findTasksByTitle(String titlePart){
+        return null;
+    }
+
+    @Override
+    public List<Task> findTasksByDoneStatus(boolean done){
+        return null;
+    }
+
+
+
+
+
+    // methods for checking if the task that is being added has all its required fields filled
+    // and whether the task was successfully added to the list
+    private boolean checkTask(Task task){
+        if(task.getDescription().isEmpty() || task.getTitle().isEmpty()
+                || !checkDeadline(task)  ){
+            return false;
+        }
+        return true;
+
+    }
+
+    private boolean checkDeadline(Task task){
+        if(!task.getDeadline().isBefore(LocalDate.now())){
+            return false;
+        }
+        return true;
+    }
+
+
+
+    //at the moment unused
+    private boolean checkIfTaskWasAdded(Task task){
+        for(Task t : tasksList){
+            if(t.getTitle().equals(task.getTitle())
+                    && t.getDeadline().equals(task.getDeadline())
+                    && t.getDescription().equals(task.getDescription())){
+
+                return true;
+            }
+
+        }
+        return false;
+    }
+
 }
